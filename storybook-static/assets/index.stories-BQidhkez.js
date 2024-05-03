fbf26297338b80a25796f6aff377d21effe68e66
Benjamin Simmers <bej1380@gmail.com>
2024-05-03 22:40:24 +1000

    add storybook static

---
+++ storybook-static/assets/index.stories-BQidhkez.js
@@ -0,0 +1,16 @@
+import{j as f,c as x,a as B}from"./index-CgSRZjIm.js";import{r as _}from"./index-DH5ua8nC.js";import"./_commonjsHelpers-Cpj98o6Y.js";const N=B(["w-full","rounded-md","font-semibold","focus:outline-none","disabled:cursor-not-allowed","transition-colors"],{variants:{variant:{solid:"bg-blue-500",outline:"border-2",ghost:"transition-colors duration-300"},size:{sm:"px-4 py-2 text-sm",md:"px-4 py-2 text-base",lg:"px-6 py-3 text-lg"},colorscheme:{primary:"text-white bg-primary-500",secondary:"text-black bg-secondary-500"}},compoundVariants:[{variant:"solid",colorscheme:"primary",className:"bg-primary-500 hover:bg-primary-600"},{variant:"outline",colorscheme:"primary",className:"text-primary-600 border-primary-500 bg-transparent hover:bg-primary-100"},{variant:"ghost",colorscheme:"primary",className:"text-primary-600 bg-transparent hover:bg-primary-100"}],defaultVariants:{variant:"solid",size:"md",colorscheme:"primary"}}),t=_.forwardRef(({variant:o,size:g,colorscheme:y,className:h,...b},v)=>f.jsx("button",{ref:v,className:x(N({variant:o,size:g,colorscheme:y,className:h})),...b}));try{t.displayName="Button",t.__docgenInfo={description:"",displayName:"Button",props:{variant:{defaultValue:null,description:"",name:"variant",required:!1,type:{name:'"solid" | "outline" | "ghost" | null'}},size:{defaultValue:null,description:"",name:"size",required:!1,type:{name:'"sm" | "md" | "lg" | null'}},colorscheme:{defaultValue:null,description:"",name:"colorscheme",required:!1,type:{name:'"primary" | "secondary" | null'}}}}}catch{}const z={title:"Components/Button",component:t,parameters:{layout:"centered"},tags:["autodocs"]},r={args:{variant:"solid",children:"Button"}},e={args:{variant:"outline",children:"Button"}},a={args:{variant:"ghost",children:"Button"}};var s,n,i;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    variant: "solid",
+    children: "Button"
+  }
+}`,...(i=(n=r.parameters)==null?void 0:n.docs)==null?void 0:i.source}}};var l,c,m;e.parameters={...e.parameters,docs:{...(l=e.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    variant: "outline",
+    children: "Button"
+  }
+}`,...(m=(c=e.parameters)==null?void 0:c.docs)==null?void 0:m.source}}};var d,p,u;a.parameters={...a.parameters,docs:{...(d=a.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    variant: "ghost",
+    children: "Button"
+  }
+}`,...(u=(p=a.parameters)==null?void 0:p.docs)==null?void 0:u.source}}};const j=["Solid","Outline","Ghost"];export{a as Ghost,e as Outline,r as Solid,j as __namedExportsOrder,z as default};
