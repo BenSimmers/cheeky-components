209515069c81721d75edc874d30c7b57e8b4a91e
Benjamin Simmers <bej1380@gmail.com>
2024-05-03 22:42:58 +1000

    remove static

--- storybook-static/assets/preview-DAfFwGNx.js
+++
@@ -1,7 +0,0 @@
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-e5cDc3dW.js","./iframe-C7QKfbFA.js","./index-DH5ua8nC.js","./_commonjsHelpers-Cpj98o6Y.js","./react-18-BT5OrVgi.js","./index-B_J8iUie.js","./_getPrototype-CEeXes8I.js","./index-DrFu-skq.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
-import{_ as t}from"./iframe-C7QKfbFA.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-NNNQARDV-e5cDc3dW.js").then(e=>e.D),__vite__mapDeps([0,1,2,3,4,5,6,7]),import.meta.url);return new r}}};export{_ as parameters};
